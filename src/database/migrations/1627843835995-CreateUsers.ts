9dc595deae0d3c6f8297d6206e1ad9056ab385a5
Daniel Trindade <daniel_brt@hotmail.com>
2021-08-07 16:57:53 -0300

    Alter user, drop username

--- src/database/migrations/1627843835995-CreateUsers.ts
+++ src/database/migrations/1627843835995-CreateUsers.ts
@@ -16,11 +16,6 @@ export class CreateUsers1627843835995 implements MigrationInterface {
                         type: "varchar"
                     },
                     {
-                        name: "username",
-                        type: "varchar",
-                        isUnique: true
-                    },
-                    {
                         name: "password",
                         type: "varchar"
                     },
